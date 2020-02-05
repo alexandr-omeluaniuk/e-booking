ed52bf5f2198cabeaea73a46db749997ef6af266
ss <starshistrelok@gmail.com>
2020-02-05 19:26:22 +0300

    Subscription restriction.

--- src/main/java/org/ss/ebooking/dao/impl/CoreDAOImpl.java
+++ src/main/java/org/ss/ebooking/dao/impl/CoreDAOImpl.java
@@ -16,16 +16,22 @@ import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Expression;
 import javax.persistence.criteria.Root;
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.Set;
 import javax.persistence.criteria.CriteriaDelete;
 import javax.persistence.criteria.Predicate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.ss.ebooking.dao.CoreDAO;
 import org.ss.ebooking.wrapper.EntitySearchRequest;
 import org.ss.ebooking.wrapper.EntitySearchResponse;
 import org.ss.ebooking.entity.DataModel;
 import org.ss.ebooking.entity.DataModel_;
+import org.ss.ebooking.entity.Subscription;
+import org.ss.ebooking.entity.TenantEntity;
+import org.ss.ebooking.entity.TenantEntity_;
+import org.ss.ebooking.service.SecurityService;
 
 /**
  * Core DAO implementation.
@@ -36,6 +42,9 @@ class CoreDAOImpl implements CoreDAO {
     /** DataModel manager. */
     @PersistenceContext
     private EntityManager em;
+    /** Security service. */
+    @Autowired
+    private SecurityService securityService;
     @Override
     @Transactional(propagation = Propagation.REQUIRED, rollbackFor = Exception.class)
     public <T extends DataModel> T create(final T entity) {
@@ -81,7 +90,8 @@ class CoreDAOImpl implements CoreDAO {
         CriteriaBuilder cb = em.getCriteriaBuilder();
         CriteriaQuery<T> criteria = cb.createQuery(cl);
         Root<T> c = criteria.from(cl);
-        criteria.select(c);
+        List<Predicate> predicates = createSearchCriteria(cb, c, cl, searchRequest);
+        criteria.select(c).where(predicates.toArray(new Predicate[0]));
         Optional.of(searchRequest.getOrderBy()).ifPresent((orderBy) -> {
             if ("asc".equals(searchRequest.getOrder())) {
                 criteria.orderBy(cb.asc(c.get(orderBy)));
@@ -94,18 +104,40 @@ class CoreDAOImpl implements CoreDAO {
                 .setMaxResults(searchRequest.getPageSize()).getResultList();
         response.setData(entities);
         // entities count
-        CriteriaQuery<Integer> criteriaCount = cb.createQuery(Integer.class);
+        CriteriaQuery<Long> criteriaCount = cb.createQuery(Long.class);
         Root<T> cCount = criteriaCount.from(cl);
-        Expression<Integer> sum = cb.max(cCount.get("id").as(Integer.class));
-        criteriaCount.select(sum);
-        List<Integer> maxList = em.createQuery(criteriaCount).getResultList();
-        Integer count = maxList.iterator().next();
-        response.setTotal(count == null ? 0 : count);
+        Expression<Long> sum = cb.count(cCount);
+        predicates = createSearchCriteria(cb, cCount, cl, searchRequest);
+        criteriaCount.select(sum).where(predicates.toArray(new Predicate[0]));
+        List<Long> maxList = em.createQuery(criteriaCount).getResultList();
+        Long count = maxList.iterator().next();
+        response.setTotal(count == null ? 0 : Integer.valueOf(String.valueOf(count)));
         return response;
     }
     // =========================================== PRIVATE ============================================================
-    private List<Predicate> createSearchCriteria() {
-        // TODO
-        return null;
+    private <T extends DataModel> List<Predicate> createSearchCriteria(CriteriaBuilder cb, Root<T> c, Class<T> clazz,
+            EntitySearchRequest searchRequest) {
+        Subscription subscription = securityService.currentUser().getSubscription();
+        List<Predicate> predicates = new ArrayList<>();
+        if (isTenantEntity(clazz)) {
+            Root<TenantEntity> cTenant = (Root<TenantEntity>) c;
+            predicates.add(cb.equal(cTenant.get(TenantEntity_.subscription), subscription));
+        }
+        return predicates;
+    }
+    /**
+     * Check if class is inherited from tenant entity.
+     * @param clazz class.
+     * @return true if class has TenantEntity super class.
+     */
+    private boolean isTenantEntity(Class clazz) {
+        Class curClass = clazz;
+        while (curClass.getSuperclass() != null) {
+            if (curClass.getSuperclass().equals(TenantEntity.class)) {
+                return true;
+            }
+            curClass = curClass.getSuperclass();
+        }
+        return false;
     }
 }
